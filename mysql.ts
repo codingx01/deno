336af8313817a6b6ff46961a62fe3a8fe7a948c4
CodingX <64512267+codingx01@users.noreply.github.com>
2020-07-01 09:03:40 +0530

    Update mysql.ts

--- mysql.ts
+++ mysql.ts
@@ -1,13 +1,11 @@
 import { Client } from "https://deno.land/x/mysql/mod.ts";
 
-const credential ={
-  hostname: "typof.ch0f92wfnknx.ap-south-1.rds.amazonaws.com",
-  username: "typof",
+const client= await new Client().connect({
+  hostname: "127.0.0.1",
+  username: "root",
   db: "deno",
-  password: "tuna1234",
-}
-
-const client= await new Client().connect(credential)
+  password: "password",
+})
 
 export { client }
 
